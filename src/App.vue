b3c57a103c2b37afd6314200d9ada84e07e9b230
Сергей Юдин <enfros2000@gmail.com>
2020-10-11 15:06:15 +0300

    - Add persistend YottaDB support - Add integration with QEWD from manuals

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,40 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  name: 'app',
+  mounted: function () {
+    var self = this
+    // monitor when QEWD is ready
+    this.$qewd.vRegistrationCallback(function (registered) {
+      self.qewdReady = registered //
+    })
+    // start the QEWD WebSockets connection ...
+    this.$qewd.vueStart()
+  },
+  data () {
+    return {
+      qewdReady: false,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    testing: function () {
+      const messageObj = {
+        type: 'test',
+        params: {
+          text: 'a Vue.js test message for QEWD'
+        }
+      }
+      const self = this
+      this.$qewd.send(messageObj, function (messageObj) {
+        self.msg = messageObj.message.text
+      })
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
